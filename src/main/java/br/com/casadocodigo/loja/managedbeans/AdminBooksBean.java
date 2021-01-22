ee16617d79afdf339db85c16a9a73d367b05bf22
dariodasilva <dariotecinfo@gmail.com>
2021-01-22 17:52:16 -0300

    Injeção da dependência Book

--- src/main/java/br/com/casadocodigo/loja/managedbeans/AdminBooksBean.java
+++ src/main/java/br/com/casadocodigo/loja/managedbeans/AdminBooksBean.java
@@ -1,7 +1,9 @@
 package br.com.casadocodigo.loja.managedbeans;
 
 import javax.enterprise.inject.Model;
+import javax.inject.Inject;
 
+import br.com.casadocodigo.loja.daos.BookDAO;
 import br.com.casadocodigo.loja.models.Book;
 
 @Model
