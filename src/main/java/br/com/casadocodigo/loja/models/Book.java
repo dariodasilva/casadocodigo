0b513093707f378d778eac7aad11ca436322bec2
dariodasilva <dariotecinfo@gmail.com>
2021-01-24 10:51:02 -0300

    Criação da model Book

--- src/main/java/br/com/casadocodigo/loja/models/Book.java
+++ src/main/java/br/com/casadocodigo/loja/models/Book.java
@@ -3,6 +3,7 @@ package br.com.casadocodigo.loja.models;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
